86586819585ab5f34038ba1b7111059f0287d11e
wooknick <wooknick@naver.com>
2022-04-07 22:33:00 +0900

    "feat" edit memo with (youtube, text)

--- src/Components/Memo.js
+++ src/Components/Memo.js
@@ -1,4 +1,6 @@
-import React, { useState, useContext, useMemo } from "react";
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/aria-role */
+import React, { useState, useContext, useMemo, useEffect } from "react";
 import styled from "styled-components";
 import { Rnd } from "react-rnd";
 import PropTypes from "prop-types";
@@ -22,13 +24,27 @@ const Wrapper = styled.div`
 `;
 
 const Content = styled.div`
-  width: calc(100% - 8px);
-  height: calc(100% - 8px);
+  width: calc(100% - 4px);
+  height: calc(100% - 4px);
   display: flex;
   justify-content: center;
   align-items: center;
   border: 1px solid #333333;
   background-color: #eeeeee;
+`;
+
+const OptionWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #333333;
+  background-color: #eeeeee;
+  position: absolute;
+  font-size: 24px;
+  &:hover {
+    cursor: pointer;
+    background-color: #cccccc;
+  }
 `;
 
 const Overlay = styled.div`
@@ -40,6 +56,28 @@ const Overlay = styled.div`
   background-color: ${props => props.bgColor};
   opacity: 0.95;
   border: 1px solid white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+`;
+
+const EditableDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  font-size: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  white-space: normal;
+  text-align: center;
+  padding: 16px;
+  background-color: #151a22;
+  color: white;
+  div {
+    width: 100%;
+  }
 `;
 
 const randomColor = () => {
@@ -55,7 +93,23 @@ const Memo = ({ cardData, updateData, removeData }) => {
    * Solution : 버그가 없는 onDrag를 통해 position을 직접 계산하여 관리함
    */
   const [position, setPosition] = useState({ x: cardData.x, y: cardData.y });
+  const [showOption, setShowOption] = useState(false);
+  const [textContent, setTextContent] = useState("Something Write Here.");
   const color = useMemo(() => randomColor(cardData.id), [cardData.id]);
+
+  let debounceT;
+
+  useEffect(() => {
+    if (!editMode) {
+      setShowOption(false);
+    }
+  }, [editMode]);
+
+  useEffect(() => {
+    if (cardData.content && cardData.content.type === "text") {
+      setTextContent(cardData.content.payload);
+    }
+  }, [cardData]);
 
   const handleDrag = (_, data) => {
     const { deltaX, deltaY } = data;
@@ -79,17 +133,54 @@ const Memo = ({ cardData, updateData, removeData }) => {
   };
 
   const handleDblClickOverlay = () => {
-    const url = prompt("복사한 유튜브 주소를 입력해주세요.") || "";
-    if (url === "delete" || url === "삭제" || url === "d") {
+    // 옵션창 확장
+    if (editMode) {
+      setShowOption(v => !v);
+    } else {
+      // 뷰 기능
+    }
+  };
+
+  const handleOption = option => {
+    if (option === "delete") {
       removeData(cardData.id);
-    }
-    const parsedUrl = parseUrl(url);
-    const newVideoId = parsedUrl.query.v;
-    if (newVideoId !== undefined) {
+    } else if (option === "youtube") {
+      const url = prompt("복사한 유튜브 주소를 입력해주세요.") || "";
+      const parsedUrl = parseUrl(url);
+      const newVideoId = parsedUrl.query.v;
+      if (newVideoId !== undefined) {
+        const newData = Object.assign(cardData);
+        newData.content = {
+          type: "youtube",
+          payload: newVideoId,
+        };
+        updateData(newData);
+      }
+      setShowOption(false);
+    } else if (option === "text") {
       const newData = Object.assign(cardData);
-      newData.videoId = newVideoId;
+      newData.content = {
+        type: "text",
+        payload: "Something Write Here.",
+      };
       updateData(newData);
+      setShowOption(false);
     }
+  };
+
+  const handleEditableKeyDown = e => {
+    if (debounceT) {
+      clearTimeout(debounceT);
+    }
+    debounceT = setTimeout(() => {
+      const newTextContent = e.target.innerHTML;
+      const newData = Object.assign(cardData);
+      newData.content = {
+        type: "text",
+        payload: newTextContent,
+      };
+      updateData(newData);
+    }, 500);
   };
 
   const defaultStyle = {
@@ -115,20 +206,73 @@ const Memo = ({ cardData, updateData, removeData }) => {
       onDragStop={handleDragStop}
       disableDragging={!editMode}
       enableResizing={editMode}
+      style={{ zIndex: showOption ? 1000 : 1 }}
     >
       <Wrapper>
         {editMode && (
-          <Overlay bgColor={color} onDoubleClick={handleDblClickOverlay} />
+          <Overlay bgColor={color} onDoubleClick={handleDblClickOverlay}>
+            <span>{cardData.content ? cardData.content.type : "Blank"}</span>
+          </Overlay>
+        )}
+        {showOption && (
+          <>
+            <OptionWrapper
+              style={{
+                width: `${MIN_WIDTH}px`,
+                height: `${MIN_HEIGHT}px`,
+                top: `-${MIN_HEIGHT}px`,
+                left: `0px`,
+              }}
+            >
+              Image
+            </OptionWrapper>
+            <OptionWrapper
+              style={{
+                width: `${MIN_WIDTH}px`,
+                height: `${MIN_HEIGHT}px`,
+                top: `-${MIN_HEIGHT}px`,
+                left: `${MIN_WIDTH}px`,
+              }}
+              onClick={() => handleOption("youtube")}
+            >
+              Youtube
+            </OptionWrapper>
+            <OptionWrapper
+              style={{
+                width: `${MIN_WIDTH}px`,
+                height: `${MIN_HEIGHT}px`,
+                top: `-${MIN_HEIGHT}px`,
+                left: `${MIN_WIDTH * 2}px`,
+              }}
+              onClick={() => handleOption("text")}
+            >
+              Text
+            </OptionWrapper>
+            <OptionWrapper
+              style={{
+                width: `${MIN_WIDTH}px`,
+                height: `${MIN_HEIGHT}px`,
+                bottom: `-${MIN_HEIGHT}px`,
+                left: `0px`,
+                color: "#ab0000",
+              }}
+              onClick={() => handleOption("delete")}
+            >
+              Delete
+            </OptionWrapper>
+          </>
         )}
         <Content>
           {cardData.content === undefined && (
-            <Overlay bgColor={color} onDoubleClick={handleDblClickOverlay} />
+            <Overlay bgColor={color} onDoubleClick={handleDblClickOverlay}>
+              <span>Blank</span>
+            </Overlay>
           )}
           {cardData.content && cardData.content.type === "youtube" && (
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/sdfffafq"
+              src={`https://www.youtube.com/embed/${cardData.content.payload}`}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -136,7 +280,12 @@ const Memo = ({ cardData, updateData, removeData }) => {
             />
           )}
           {cardData.content && cardData.content.type === "text" && (
-            <div>{cardData.content.payload}</div>
+            <EditableDiv
+              contentEditable="true"
+              role="editor"
+              onKeyDown={handleEditableKeyDown}
+              dangerouslySetInnerHTML={{ __html: textContent }}
+            />
           )}
         </Content>
       </Wrapper>
